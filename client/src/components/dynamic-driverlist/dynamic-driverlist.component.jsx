46eeff7e89d4d4d16b1b1cf4de61e9f1179b200c
Anonster <sameerkatija@gmail.com>
2021-04-28 22:11:18 +0500

    Added User Dissconnected Feature

--- client/src/components/dynamic-driverlist/dynamic-driverlist.component.jsx
+++ client/src/components/dynamic-driverlist/dynamic-driverlist.component.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./dynamic-driverlist.styles.scss";
 import DynamicDriver from "../dynamic-driver/dynamic-driver.componet";
 import { connect } from "react-redux";
+import { setSnackbar } from "../../redux/snackbar/snackbar.action";
+import axios from "axios";
 
-const DynamicDriverList = ({ currentDrivers }) => {
+const DynamicDriverList = ({
+  currentDrivers,
+  dissconnectedUser,
+  setSnackbar,
+}) => {
+  useEffect(() => {
+    axios
+      .get(`/api/users/${dissconnectedUser}`)
+      .then((res) => {
+        const { firstName, lastName } = res.data;
+        setSnackbar(true, "error", `${firstName} ${lastName} is dissconnected`);
+      })
+      .catch((error) => console.log(error.message));
+    console.log(dissconnectedUser);
+  }, [dissconnectedUser]);
   return (
-    <div className="drivers-container">
+    <div className='drivers-container'>
       {currentDrivers ? (
-        <div className="drivers-flex">
+        <div className='drivers-flex'>
           {currentDrivers.map((element, index) => (
             <DynamicDriver key={index} userinfo={element} />
           ))}
@@ -21,6 +37,11 @@ const DynamicDriverList = ({ currentDrivers }) => {
 
 const mapStateToProps = (state) => ({
   currentDrivers: state.drivers.currentDrivers,
+  dissconnectedUser: state.drivers.disconnectedDriver,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setSnackbar: (snackbarOpen, snackbarType, snackbarMessage) =>
+    dispatch(setSnackbar(snackbarOpen, snackbarType, snackbarMessage)),
 });
 
-export default connect(mapStateToProps, null)(DynamicDriverList);
+export default connect(mapStateToProps, mapDispatchToProps)(DynamicDriverList);
