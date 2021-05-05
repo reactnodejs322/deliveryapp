a3d4abefe90a121afefe65d58c7b1bbb6b407b52
reactnodejs32 <reactnodejs32@gmail.com>
2021-05-05 15:45:14 -0400

    firekey change

--- client/src/redux/util/Key.js
+++ client/src/redux/util/Key.js
@@ -1,10 +1,9 @@
 export const firekey = {
-  apiKey: "AIzaSyBiH8-9EmbbSrir5KaJnvq83fhZw7IubWI",
-  authDomain: "products-dbz.firebaseapp.com",
-  databaseURL: "https://products-dbz.firebaseio.com",
-  projectId: "products-dbz",
-  storageBucket: "products-dbz.appspot.com",
-  messagingSenderId: "654682328363",
-  appId: "1:654682328363:web:7d2f42b5c434db2616496e",
-  measurementId: "G-9T4VPNFD40",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
